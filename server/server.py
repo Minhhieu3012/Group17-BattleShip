992550ea988e3fc070a5b609c3bf8e902ba38093
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-08-16 09:33:10 +0700

    feature: done function start_server 16/8

--- server/server.py
+++ server/server.py
@@ -33,3 +33,15 @@ def handle_client(conn,addr):
                     rooms.pop(room.room_id, None) # Xóa room nếu không còn người chơi
             broadcast_room(room,{"action":"opponent_left"}) # Thông báo người chơi đã rời khỏi phòng
         conn.close() # Đóng kết nối
+
+def start_server(host='127.0.0.1',port=5000):
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind((host,port))
+    s.listen()
+    print(f"Server running on {host}:{port}")
+    while True:
+        conn,addr=s.accept()
+        threading.Thread(target=handle_client,args=(conn,addr)).start() # Mỗi kết nối sẽ chạy trên một thread riêng
+
+if __name__=="__main__":
+    start_server()
